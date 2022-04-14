ba171d3437bc9185ae57249f84f2e983778db2c6
Ales Web <aleswebgit@gmail.com>
2022-04-14 12:19:54 +0200

    change color when change img of coffee

--- code.js
+++ code.js
@@ -4,10 +4,11 @@ const imgFirst = document.getElementById('change-img-first');
 const imgSecond = document.getElementById('change-img-second');
 const imgThird = document.getElementById('change-img-third');
 
-function changeImg(img) {
+function changeImg(img, color) {
   bigImg.src = img;
+  document.documentElement.setAttribute('data-color', color);
 }
 
-imgFirst.addEventListener('click', () => { changeImg('./img/img1.png'); });
-imgSecond.addEventListener('click', () => { changeImg('./img/img2.png'); });
-imgThird.addEventListener('click', () => { changeImg('./img/img3.png'); });
+imgFirst.addEventListener('click', () => { changeImg('./img/img1.png', 'green'); });
+imgSecond.addEventListener('click', () => { changeImg('./img/img2.png', 'orange'); });
+imgThird.addEventListener('click', () => { changeImg('./img/img3.png', 'pink'); });
