d9e1cb4fe0b14cf37f1399c241f2c326936afe38
Ales Web <aleswebgit@gmail.com>
2022-04-14 11:00:47 +0200

    add js

---
+++ code.js
@@ -0,0 +1,5 @@
+const bigImg = document.getElementById('big-img');
+
+const imgFirst = document.getElementById('change-img-first');
+const imgSecond = document.getElementById('change-img-second');
+const imgThird = document.getElementById('change-img-third');
