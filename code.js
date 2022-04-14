1131c5399f86c20c937a56fd66da07b6ae63870e
Ales Web <aleswebgit@gmail.com>
2022-04-14 11:27:12 +0200

    change big image when click a coffe

--- code.js
+++ code.js
@@ -3,3 +3,11 @@ const bigImg = document.getElementById('big-img');
 const imgFirst = document.getElementById('change-img-first');
 const imgSecond = document.getElementById('change-img-second');
 const imgThird = document.getElementById('change-img-third');
+
+function changeImg(img) {
+  bigImg.src = img;
+}
+
+imgFirst.addEventListener('click', () => { changeImg('./img/img1.png'); });
+imgSecond.addEventListener('click', () => { changeImg('./img/img2.png'); });
+imgThird.addEventListener('click', () => { changeImg('./img/img3.png'); });
